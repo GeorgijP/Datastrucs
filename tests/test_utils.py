d7ebc140d96fe89d66bb87b1bae05b2e578644d5
GeorgijP <122865026+GeorgijP@users.noreply.github.com>
2023-03-03 12:18:14 +0400

    Create test_utils.py
    add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+import pytest
+from utils.utils import Stack
+
+stack = Stack()
+stack.push('data1')
+stack.push('data2')
+stack.push('data3')
+
+def test_Stack():
+    assert stack.top.data == "data3"
+    assert stack.top.next_node.data == "data2"
+    assert stack.top.next_node.next_node.data == "data1"
