ba1242c9dc8ba6a93599fa24919f7c6362350e16
GeorgijP <122865026+GeorgijP@users.noreply.github.com>
2023-03-11 12:46:27 +0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,11 +1,1 @@
-from utils.stack import Queue
 
-queue = Queue()
-queue.enqueue('data1')
-queue.enqueue('data2')
-queue.enqueue('data3')
-
-print(queue.dequeue())
-# print(queue.dequeue())
-# print(queue.dequeue())
-# print(queue.dequeue())
