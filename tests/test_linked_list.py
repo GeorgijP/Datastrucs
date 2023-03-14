2b5933162d6f5d48d0f37f9a309d5d1e4ea62b0c
GeorgijP <georgij.puzanov@inbox.ru>
2023-03-14 12:57:24 +0400

    исправил тесты test_linked_list

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,4 +1,5 @@
-import unittest
+from unittest import TestCase
+
 from utils.linked_list import LinkedList
 
 ll = LinkedList()
@@ -7,16 +8,16 @@ ll.insert_at_end({"id": 2})
 ll.insert_at_end({"id": 3})
 ll.insert_beginning({"id": 0})
 
+class Test_ll(TestCase):
+    def test_LinkedList(self):
+        self.assertEqual(ll.print_ll(), print(" {'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"))
 
-def test_LinkedList(self):
-    self.assertEqual(ll.print_ll(), ' {"id": 0} -> {"id": 1} -> {"id": 2} -> {"id": 3} -> None')
+    def test_to_list(self):
+        self.assertEqual(print(ll.to_list()), print("[{'id': 0}, {'id': 1}, {'id': 2}, {'id': 3}]"))
 
-def test_to_list(self):
-    self.assertEqual(print(ll.to_list()), "[{'id': 0}, {'id': 1}, {'id': 2}, {'id': 3}]")
-
-def test_get_data_by_id(self):
-    self.assertEqual(print(ll.get_data_by_id(0)), "{'id': 0}")
-    self.assertEqual(print(ll.get_data_by_id(1)), "{'id': 1}")
-    self.assertEqual(print(ll.get_data_by_id(2)), "{'id': 2}")
-    self.assertEqual(print(ll.get_data_by_id(3)), "{'id': 3}")
-    self.assertEqual(print(ll.get_data_by_id(4)), None)
+    def test_get_data_by_id(self):
+        self.assertEqual(print(ll.get_data_by_id(0)), print("{'id': 0}"))
+        self.assertEqual(print(ll.get_data_by_id(1)), print("{'id': 1}"))
+        self.assertEqual(print(ll.get_data_by_id(2)), print("{'id': 2}"))
+        self.assertEqual(print(ll.get_data_by_id(3)), print("{'id': 3}"))
+        self.assertEqual(print(ll.get_data_by_id(4)), None)
